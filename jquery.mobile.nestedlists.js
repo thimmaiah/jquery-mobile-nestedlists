4b3b7e89158db079201f2a9dacd5a04abf1d14b2
Mohith Thimmaiah <thimmaiah@gmail.com>
2014-07-24 23:13:41 -0400

    Fixed issues with JQM 1.4

--- jquery.mobile.nestedlists.js
+++ jquery.mobile.nestedlists.js
@@ -7,12 +7,14 @@
 			content: "<div class='ui-content'></div>"
 		},
 		_create: function(){
+			//console.log("nestedList: _create");
 			this._super();
 			if( this.options.childPages ) {
 				this._setupChildren();
 			}
 		},
 		_setupChildren: function() {
+			//console.log("nestedList: _setupChildren");
 			this._attachBindings();
 			this.element.find( "ul" )
 				.css( "display","none" )
@@ -20,6 +22,7 @@
 				.addClass("ui-btn ui-btn-icon-right ui-icon-carat-r");
 		},
 		_attachBindings: function() {
+			console.log("nestedList: _attachBindings");
 			this._on({
 				"click": "_handleSubpageClick"
 			});
@@ -36,14 +39,27 @@
 			});
 		},
 		_handleSubpageClick: function( event ) {
+			console.log("nestedList: _handleSubpageClick called");
 			if( $(event.target).closest( "li" ).children( "ul" ).length == 0 ) {
-				return;
+				console.log("nestedList: No children");
+				return;				
 			}
 			this.opening = true;
 			this.newPage = $( this.options.page ).uniqueId();
-			this.nestedList  = $( event.target ).children( "ul" )
+			//console.log("nestedList: event.target = " + $( event.target ).html());
+		
+			if ($( event.target ).is("li")) {
+				li = $( event.target );
+			} else {
+				li = $( event.target ).closest("li");
+			}
+		
+			this.nestedList  = li.children( "ul" )
 				.clone().attr( "data-" + $.mobile.ns + "role", "listview" )
 				.css( "display", "block" );
+			
+			//console.log("nestedList: children = " + this.nestedList.html());
+			
 			this.pageName = (
 				$( event.target.childNodes[0] ).text().replace(/^\s+|\s+$/g, '').length > 0 )?
 				$( event.target.childNodes[0] ).text() : $( event.target.childNodes[1] ).text();
@@ -55,10 +71,16 @@
 			).append(
 				$( this.options.content )
 			).find( "div.ui-content" ).append( this.nestedList );
-
+			
+			this.newPage.attr("id", this.pageID);
+			
 			$( "body" ).append( this.newPage );
-
-			$( "body" ).pagecontainer( "change", "#" + this.pageID );
+			console.log("nestedList: pageID = " + this.pageID);
+			// console.log("nestedList: page = " + $("#"+this.pageID).html());
+				
+			//$("body").pagecontainer("change", "#" + this.pageID);
+			window.location.hash = this.pageID;
+		    //$.mobile.initializePage();
 		}
 	});
 })( jQuery, this );
